7c01c878ce4ee06a149f9cb88cd915a98c9e1a40
Crence Cobbs <crencecobbs3@gmail.com>
2025-08-11 07:57:40 -0700

    chore: add Arduino folder structure and starter sketches

---
+++ arduino/milestone-tracker/milestone-tracker.ino
@@ -0,0 +1,2 @@
+void setup() { Serial.begin(115200); }
+void loop()  { Serial.println("milestone-tracker alive"); delay(1000); }
---
+++ arduino/vault-controller/vault-controller.ino
@@ -0,0 +1,2 @@
+void setup() { Serial.begin(115200); }
+void loop()  { Serial.println("vault-controller alive"); delay(1000); }
